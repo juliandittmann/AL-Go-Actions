98b3c584f0bd11ae8eaf1c3de64d1a2def25f0a9
Julian Dittmann <julian.dittmann@mail.de>
2022-06-08 14:06:50 +0200

    removed

--- ReadSecrets/ReadSecretsHelper.psm1
+++ ReadSecrets/ReadSecretsHelper.psm1
@@ -15,19 +15,6 @@ function MaskValueInLog {
     )
 
     Write-Host "::add-mask::$value"
-
-    $val2 = ""
-    $value.ToCharArray() | ForEach-Object {
-        $chint = [int]$_
-        if ($chint -lt 32 -or $chint -gt 126 ) {
-            throw "Secret $key contains characters, which are not supported in secrets in AL-Go for GitHub. This exception is thrown to avoid that the secret is revealed in the log."
-        }
-        else {
-            $val2 += $script:escchars[$chint-32]
-        }
-    }
-
-    Write-Host "::add-mask::$val2"
 }
 
 function GetGithubSecret {
